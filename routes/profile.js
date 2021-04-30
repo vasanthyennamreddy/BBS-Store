7a9e138cca3a8a9088cb5e457f2440faf2969091
Tez003 <jsv10@iitbbs.ac.in>
2021-04-30 20:27:58 +0530

    Profile error correction

--- routes/profile.js
+++ routes/profile.js
@@ -2,32 +2,42 @@ const express = require('express');
 const router  = express.Router();
 const Product = require('../models/product');
 const Order = require('../models/orders');
-const { getPreviousOrders } = require('../models/orders');
+
 
 router.get('', (req,res) => {
+
+    if(req.session.isLoggedIn){
     var orders;
+    var pending_orders;
     Order.getPreviousOrders(req.session.userid)
     .then (result =>{
         orders = result[0];
-        console.log(orders);
+        Order.getPendingOrders(req.session.userid)
+        .then(result =>{
+            pending_orders = result[0];
+            Product.getBySeller(req.session.userid)
+                .then( result => {
+                    addedproducts = result[0];
+                
+                    
+                    return res.render('profile',{title:"Profile", sess: req.session,useraddedproducts : addedproducts, orders: orders, pending_orders: pending_orders});
+                })
+                .catch( err => {
+                    console.log(err);
+                });
+        });
     });
 
-    var pending_orders;
-    Order.getPendingOrders(req.session.userid)
-    .then(result =>{
-        pending_orders = result[0];
+    
+   
 
-    });
 
-    Product.getBySeller(req.session.userid)
-    .then( result => {
-        addedproducts = result[0];
-        return res.render('profile',{title:"Profile", sess: req.session,useraddedproducts : addedproducts, orders, pending_orders});
-    })
-    .catch( err => {
-        console.log(err);
-    });
+    
+}
 
+    else{
+        res.redirect('login')
+    }
     
 });
 
